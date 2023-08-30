44a2f1dc1cf195b9b52618dcd4eae41b4b09f7c7
galexbh <galexbh@protonmail.com>
2023-08-29 20:30:17 -0600

    fix(disgusting iframe): This registration page is disgusting

--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,48 @@
-document.getElementById('applyBtn').addEventListener('click', function() {
-    const selectedValue = document.getElementById('valueSelect').value;
+document.getElementById("applyBtn").addEventListener("click", function () {
+  const selectedValue = document.getElementById("valueSelect").value;
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        const tab = tabs[0];
-        if (tab) {
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: setSelectValues,
-                args: [selectedValue]
-            });
-        } else {
-            console.error("No se encontró ninguna pestaña activa.");
-        }
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tab = tabs[0];
+    if (tab) {
+      chrome.scripting
+        .executeScript({
+          target: { tabId: tab.id },
+          function: setSelectValues,
+          args: [selectedValue],
+        })
+        .then(() => {
+          return chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+          });
+        })
+        .catch((error) => {
+          console.error("Error injecting scripts:", error);
+        });
+    } else {
+      console.error("No active tab found");
+    }
+  });
 });
 
+
 function setSelectValues(value) {
-    for (const select of document.getElementsByTagName('select')) {
-        select.value = value;
-        select.onchange?.();
+    const iframe = document.querySelector('iframe');
+    const iframeContent = iframe.contentDocument || iframe.contentWindow.document;
+    const selects = iframeContent.querySelectorAll('.gradeA select');
+
+    for (const select of selects) {
+        for (const option of select.options) {
+            if (option.value == value) {
+                option.selected = true;
+            } else {
+                option.selected = false;
+            }
+        }
+        
+        const event = new Event('change', {
+            'bubbles': true,
+            'cancelable': true
+        });
+        select.dispatchEvent(event);
     }
 }
